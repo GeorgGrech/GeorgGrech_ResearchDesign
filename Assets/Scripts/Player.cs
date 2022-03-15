a10bf75406587b04d9a3dc1ed95b07bd62f8d13a
GeorgGrech <56017225+GeorgGrech@users.noreply.github.com>
2022-03-15 18:38:59 +0100

    Basic player script and A* import
    -Created basic player script with ChangeHealth function linked to GameManager.PlayerHealth
    -Imported A* package to be used later for enemy movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    void ChangeHealth(int amount)
+    {
+        gameManager.PlayerHealth += amount;
+    }
+}
