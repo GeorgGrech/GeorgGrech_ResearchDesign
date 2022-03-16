9f4748764dac9bcf1fde7003c17e5266af8977fc
GeorgGrech <56017225+GeorgGrech@users.noreply.github.com>
2022-03-16 12:37:01 +0100

    Enemy Pathfinding
    -Added A* object with pathfinding script to Sample Level
    -Added A* appropriate A* scripts to enemy
    -Set pathfinding to enable within distance to player
    -Reworked enemy to fix collision issue with A*
    -Set Enemy weapons as non-player weapons
    -Set M4/Shotgun as prefabs again alongside Weapons object containing both

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,25 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Enemy : ShootableObject
 {
+    public Transform player;
+
     [SerializeField] private int objectHealth = 100;
+
+    //A* variables
+    private Seeker seeker;
+    private AIPath aiPath;
+    private AIDestinationSetter destinationSetter;
+
+    //Player detection variables
+    [SerializeField] private float detectDistance = 10;
+    [SerializeField] private float shootAngle = 30;
+    public LayerMask obstacleMask; //Obstacles that block enemy vision
+
+
+    #region Inherited methods
     // Start is called before the first frame update
     protected override void Start()
     {
         base.health = objectHealth;
         base.Start();
+
+        player = GameObject.FindGameObjectWithTag("Player").transform; //Find player
+        //weapon = weaponObject.GetComponent<Weapon>();
+        seeker = GetComponent<Seeker>();
+        aiPath = GetComponent<AIPath>();
+        destinationSetter = GetComponent<AIDestinationSetter>();
+
+        destinationSetter.target = player;
     }
 
     protected override void ChangeHealth(int amount)
     {
         base.ChangeHealth(amount);
     }
+    #endregion
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (DetectPlayer()/*&& !stunActive && !isDying*/)
+        {
+            //StopAllCoroutines(); //Stop everything to follow player
+            AStarEnable(true);
+            //animator.SetBool("isFollowing", true);
+
+
+            /*if (PlayerInSight(shootAngle) && !stunActive)
+            {
+                weapon.isFiring = true;
+            }
+            else weapon.isFiring = false;*/
+        }
+    }
+
+    bool DetectPlayer() //Spot player in field of view
+    {
+        if (player) //if player hasn't been destroyed
+        {
+            if (Vector3.Distance(transform.position, player.position) < detectDistance) //if within view distance
+            {
+
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public void AStarEnable(bool enable) //Enables tracking
+    {
+        //isFollowingPlayer = enable;
+        seeker.enabled = enable;
+        aiPath.enabled = enable;
+        destinationSetter.enabled = enable;
     }
 }
